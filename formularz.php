9837d163b81aab63e75b3516a9202ecd8e43de33
fajus <gucio0021@wp.pl>
2015-05-07 16:40:00 +0200

    Summary
    Opis

---
+++ formularz.php
@@ -0,0 +1,16 @@
+	#komunikat#
+	<form action="#strona#" method="post" id="formularz">
+		<div>
+			<input type="text" id="mail" name="mail" value="#mail#" placeholder="Adres email" />
+		</div>
+		<div>
+			<input type="text" id="temat" name="temat" value="#temat#" placeholder="Temat" />
+		</div>
+		<div>
+			<textarea cols="47" rows="5" id="tresc" name="tresc" placeholder="Treść" >#tresc#</textarea>
+		</div>
+		<div>
+			<input type="submit" value="Wyślij" />
+		</div>
+		
+	</form>
